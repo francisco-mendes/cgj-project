a60eece43e507123ee9e3e86c928296dc6e184c9
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-10-30 21:50:36 +0000

    Add Shader and ShaderProgram classes
    Implement RAII class Shader
    - Provide enum for different Shader types
    - Read shader from file or string
    - Compile Shader
    - Destruct Shader when out of scope
    
    Implement Global class ShaderProgram
    - Move Vertices, Colors, UBO_BP constants inside class
    - Rename Vertices to Position, UBO_BP to Camera
    - Link Program from multiple shaders
    - Cleanup in callback on exit
    - No RAII, no destructor because of global lifetime
    - Inputs and uniforms are still hardcoded
    - Only one ShaderProgram should exist

---
+++ Shader.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+
+#include <fstream>
+#include <optional>
+#include <string>
+
+#include <GL/glew.h>
+
+class Shader
+{
+public:
+    enum Type : GLenum
+    {
+        Vertex = GL_VERTEX_SHADER,
+        TesselationControl = GL_TESS_CONTROL_SHADER,
+        TesselationEvaluation = GL_TESS_EVALUATION_SHADER,
+        Geometry = GL_GEOMETRY_SHADER,
+        Fragment = GL_FRAGMENT_SHADER,
+        Compute = GL_COMPUTE_SHADER
+    };
+
+private:
+    Type   shader_type_;
+    GLuint shader_id_;
+
+public:
+    Shader(Type shader_type, std::string_view);
+    static Shader fromFile(Type shader_type, std::string const& filepath);
+
+    Shader(Shader const&) = delete;
+    Shader(Shader&&)      = default;
+
+    Shader& operator=(Shader const&) = delete;
+    Shader& operator=(Shader&&)      = default;
+
+    ~Shader();
+
+    [[nodiscard]] Type   type() const;
+    [[nodiscard]] GLuint id() const;
+};
+
+class ShaderProgram
+{
+public:
+    constexpr static GLuint Position = 0;
+    constexpr static GLuint Colors   = 1;
+
+    constexpr static GLuint Camera = 0;
+
+private:
+    GLuint program_id_;
+    GLint  model_id_, camera_id_;
+
+public:
+    ShaderProgram() = default;
+    ShaderProgram(std::initializer_list<Shader> shaders);
+
+    void cleanup();
+
+    [[nodiscard]] GLuint programId() const;
+    [[nodiscard]] GLint  modelId() const;
+    [[nodiscard]] GLint  cameraId() const;
+};
+
