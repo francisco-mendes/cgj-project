b37dc07cb001d38552422b4a0ce862f988a54bb6
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-11-03 02:37:04 +0000

    Implement Quaternions
    Add conversions to and from
        angle and axis
        components
        real and pure parts
    Add matrix conversion
    Implement quadrance, magnitude, abs, norm, clean ops
    Implement conjugate and inverse ops
    Implement lerp and slerp ops
    Implement +, -, *, ==, !=, << operators

---
+++ Math/Quaternion.cpp
@@ -0,0 +1,124 @@
+﻿#include "Quaternion.h"
+
+Quaternion Quaternion::fromVector(Vector4 const components)
+{
+    auto const [t, x, y, z] = components;
+    return {t, x, y, z};
+}
+
+Quaternion Quaternion::fromParts(float const t, Vector3 const vec)
+{
+    auto const [x, y, z] = vec;
+    return {t, x, y, z};
+}
+
+Quaternion Quaternion::fromAngleAxis(Radians const angle, Vector3 const axis)
+{
+    auto const t         = cos(angle / 2.f);
+    auto const scale     = sin(angle / 2.f);
+    auto const [x, y, z] = axis.normalized() * scale;
+
+    Quaternion const res = {t, x, y, z};
+    return res.cleaned().normalized();
+}
+
+std::pair<Radians, Vector4> Quaternion::toAngleAxis() const
+{
+    auto const [t, v] = normalized().toParts();
+    auto const angle  = 2.f * std::acos(t);
+    auto const scale  = std::sqrt(1.f - t * t);
+
+    auto const [x, y, z] = scale < Epsilon ? Vector3 {1.f, 0.f, 0.f} : v * (1 / scale);
+
+    return {angle, {x, y, z, 1}};
+}
+
+std::pair<float, Vector3> Quaternion::toParts() const { return {t, {x, y, z}}; }
+
+Vector4 Quaternion::toVector() const { return {t, x, y, z}; }
+
+Matrix4 Quaternion::toMatrix() const
+{
+    auto const [t, x, y, z] = normalized();
+
+    auto const tx = 2.f * t * x;
+    auto const ty = 2.f * t * y;
+    auto const tz = 2.f * t * z;
+
+    auto const xx = 2.f * x * x;
+    auto const xy = 2.f * x * y;
+    auto const xz = 2.f * x * z;
+
+    auto const yy = 2.f * y * y;
+    auto const yz = 2.f * y * z;
+    auto const zz = 2.f * z * z;
+
+    return {
+        1.f - yy - zz, xy - tz, xz + ty, 0,
+        xy + tz, 1.f - xx - zz, yz - tx, 0,
+        xz - ty, yz + tx, 1.f - xx - yy, 0,
+        0, 0, 0, 1
+    };
+}
+
+float Quaternion::quadrance() const { return toVector().quadrance(); }
+float Quaternion::magnitude() const { return std::sqrt(quadrance()); }
+
+Quaternion Quaternion::absolute() const { return fromVector(toVector().absolute()); }
+Quaternion Quaternion::normalized() const { return fromVector(toVector().normalized()); }
+Quaternion Quaternion::cleaned() const { return fromVector(toVector().cleaned()); }
+
+Quaternion Quaternion::conjugated() const { return {t, -x, -y, -z}; }
+Quaternion Quaternion::inverted() const { return conjugated() * (1.f / quadrance()); }
+
+Quaternion Quaternion::lerp(float const scale, Quaternion const start, Quaternion const end)
+{
+    auto const angle   = start.toVector() * end.toVector();
+    auto const k_start = 1.f - scale;
+    auto const k_end   = angle > 0 ? scale : -scale;
+    return (k_start * start + k_end * end).normalized();
+}
+
+Quaternion Quaternion::slerp(float const scale, Quaternion const start, Quaternion const end)
+{
+    auto const angle   = start.toVector() * end.toVector();
+    auto const k_start = std::sin((1.f - scale) * angle) / std::sin(angle);
+    auto const k_end   = std::sin(scale * angle) / std::sin(angle);
+    return (k_start * start + k_end * end).normalized();
+}
+
+Quaternion operator+(Quaternion const left, Quaternion const right)
+{
+    return Quaternion::fromVector(left.toVector() + right.toVector());
+}
+
+Quaternion operator-(Quaternion const left, Quaternion const right)
+{
+    return Quaternion::fromVector(left.toVector() - right.toVector());
+}
+
+Quaternion operator*(Quaternion const left, Quaternion const right)
+{
+    return {
+        left.t * right.t - left.x * right.x - left.y * right.y - left.z * right.z,
+        left.t * right.x + left.x * right.t + left.y * right.z - left.z * right.y,
+        left.t * right.y - left.x * right.z + left.y * right.t + left.z * right.x,
+        left.t * right.z + left.x * right.y - left.y * right.x + left.z * right.t,
+    };
+}
+
+Quaternion operator*(Quaternion const left, float const right)
+{
+    return Quaternion::fromVector(left.toVector() * right);
+}
+
+Quaternion operator*(float const left, Quaternion const right) { return right * left; }
+
+bool operator==(Quaternion const left, Quaternion const right) { return left.toVector() == right.toVector(); }
+bool operator!=(Quaternion const left, Quaternion const right) { return !(left == right); }
+
+std::ostream& operator<<(std::ostream& os, Quaternion const q)
+{
+    os << '(' << q.t << ", " << q.x << "i, " << q.y << "j, " << q.z << 'k)';
+    return os;
+}
---
+++ Math/Quaternion.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "Vector.h"
+#include "Matrix.h"
+
+struct Quaternion
+{
+    static Quaternion fromVector(Vector4 components);
+    static Quaternion fromParts(float t, Vector3 vec);
+    static Quaternion fromAngleAxis(Radians angle, Vector3 axis);
+
+    std::pair<Radians, Vector4> toAngleAxis() const;
+    std::pair<float, Vector3>   toParts() const;
+    Vector4                     toVector() const;
+    Matrix4                     toMatrix() const;
+
+    float quadrance() const;
+    float magnitude() const;
+
+    Quaternion absolute() const;
+    Quaternion normalized() const;
+    Quaternion cleaned() const;
+
+    Quaternion conjugated() const;
+    Quaternion inverted() const;
+
+    static Quaternion lerp(float scale, Quaternion start, Quaternion end);
+    static Quaternion slerp(float scale, Quaternion start, Quaternion end);
+
+    float t, x, y, z;
+};
+
+Quaternion operator+(Quaternion left, Quaternion right);
+Quaternion operator-(Quaternion left, Quaternion right);
+Quaternion operator*(Quaternion left, Quaternion right);
+
+Quaternion operator*(Quaternion left, float right);
+Quaternion operator*(float left, Quaternion right);
+
+bool operator==(Quaternion left, Quaternion right);
+bool operator!=(Quaternion left, Quaternion right);
+
+std::ostream& operator<<(std::ostream& os, Quaternion q);
