1c7839f16dbbf6691f6856e20a8cbd1bee4d3a3c
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-11-06 17:05:06 +0000

    Refactor Camera api
    Add MousePosition for grouping cursor screen coordinates
    
    Add Rotation type alias for alternating between rotation models
    Add Projection enum for tracking camera projection
    
    Change constructor to use a fixed facing vector and a distance
    Rename center to focus
    Add Swap perspective and rotation model methods
    Add drag and passive rotation methods
    Rename camBinds to update, pass in changes in input from controller
    Add a pseudo destructor cleanup method
    Add rotation matrix and rotation calculation helper methods
    
    Add Controller for mouse input
    - Start, rotate and finish Drag methods for rotations
    - Scroll method for zooming
    - Update to manage controls with render loop
    - Drag, scroll delta getters for changes in input

--- Camera.h
+++ Camera.h
@@ -1,32 +1,76 @@
 #pragma once
+
+#include <variant>
 #include <GL/glew.h>
 
+
 #include "Math/Matrix.h"
-#include "Camera.h"
+#include "Math/Quaternion.h"
+#include "Math/Vector.h"
 
-struct Camera {
-	Vector3 position;
-	Vector3 center;
-	Vector3 up;
-	
-	bool ortho;
-	
-	float camSpeed;
-	int mForward;
-	int mRight;
-	int mUp;
-	
-	double oldX, oldY;
-	bool b = true;
-	
-	GLuint vbo[1];
-	
-	Camera() = default;
-	Camera(Vector3 pos, Vector3 c, Vector3 u, float cSpeed, GLuint viewId);
+class Controller;
 
-	void moveCam();
-	void rotateCamera(double x, double y);
-	Matrix4 CamView();
-	Matrix4 CamPerpective();
-	void camBinds();
+struct MousePosition
+{
+    double x, y;
+
+    operator Vector2() const;
+};
+
+class Camera
+{
+public:
+    using Rotation = std::variant<Matrix4, Quaternion>;
+
+    enum Projection: bool
+    {
+        Orthogonal,
+        Perspective
+    };
+
+private:
+    GLuint     buffer_id_;
+    Projection projection_;
+    Vector3    focus_,    up_;
+    float      distance_, change_;
+    Rotation   rotation_;
+
+public:
+    Camera() = default;
+    Camera(float distance, Vector3 focus, Vector3 up, GLuint view_id);
+
+    void swapProjection();
+    void swapRotationMode();
+
+    void rotate(Controller const& controller);
+    void rotate(double frame_delta);
+
+    void update(Vector2 drag_delta, float zoom);
+    void cleanup();
+
+private:
+    [[nodiscard]] Matrix4  rotationMatrix(Vector2 drag_delta) const;
+    [[nodiscard]] Rotation fullRotation(Vector2 drag_delta) const;
+};
+
+class Controller
+{
+    bool    dragging_;
+    Vector2 drag_start_, drag_now_;
+    double  scroll_;
+    Camera  camera_;
+public:
+    Controller() = default;
+    Controller(Camera&& camera);
+
+    void startDrag(MousePosition mouse_position);
+    void rotateDrag(MousePosition mouse_position);
+    void finishDrag();
+    void scroll(double by);
+    void update(double frame_delta);
+
+    [[nodiscard]] Vector2 dragDelta() const;
+    [[nodiscard]] bool    isDragging() const;
+    [[nodiscard]] float   scrollDelta(double frame_delta);
+    [[nodiscard]] Camera& camera();
 };
