19aba4f98f12a2e9cadc4eb40bb55364c6d1aaa3
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-11-27 17:39:33 +0000

    Add Transform and Animation types
    Transform controls object's position, rotation and scaling
    Can interpolate between two transformations
    
    Animation interpolates between two transformations

---
+++ Render/Transform.cpp
@@ -0,0 +1,58 @@
+﻿#include "Transform.h"
+
+namespace render
+{
+    Matrix4 Transform::toMatrix() const
+    {
+        return Matrix4::translation(position) * rotation.toRotationMatrix() * Matrix4::scaling(scaling);
+    }
+
+    Transform Transform::interpolate(float const scale, Transform const& start, Transform const& end)
+    {
+        auto const pos = start.position + (end.position - start.position) * scale;
+        auto const rot = Quaternion::slerp(scale, start.rotation, end.rotation);
+        auto const sca = start.scaling + (end.scaling - start.scaling) * scale;
+        return {pos, rot, sca};
+    }
+
+    Animation::Animation(Transform start, Transform target)
+        : start_ {std::move(start)},
+          target_ {std::move(target)},
+          state_ {0.0},
+          active_ {false},
+          reversed_ {false}
+    {}
+
+    Transform Animation::advance(double const elapsed_sec)
+    {
+        constexpr auto Speed = 0.5;
+        if (reversed_)
+        {
+            state_ -= elapsed_sec * Speed;
+            if (state_ <= 0)
+            {
+                state_    = 0;
+                active_   = false;
+                reversed_ = false;
+                return start_;
+            }
+            return Transform::interpolate(state_, start_, target_);
+        }
+
+        state_ += elapsed_sec * Speed;
+        if (state_ >= 1)
+        {
+            state_    = 1;
+            active_   = false;
+            reversed_ = true;
+            return target_;
+        }
+        return Transform::interpolate(state_, start_, target_);
+    }
+
+    bool Animation::active() const { return active_; }
+    bool Animation::reversed() const { return reversed_; }
+
+    void Animation::activate() { active_ = true; }
+    void Animation::reverse() { reversed_ = !reversed_; }
+}
---
+++ Render/Transform.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include "../Math/Matrix.h"
+#include "../Math/Quaternion.h"
+#include "../Math/Vector.h"
+
+namespace render
+{
+    struct Transform
+    {
+        Matrix4 toMatrix() const;
+
+        Vector3    position = Vector3::filled(0);
+        Quaternion rotation = Quaternion::identity();
+        Vector3    scaling  = Vector3::filled(1);
+
+        [[nodiscard]] static Transform interpolate(float scale, Transform const& start, Transform const& end);
+    };
+
+    class Animation
+    {
+        Transform start_;
+        Transform target_;
+
+        double state_;
+
+        bool active_;
+        bool reversed_;
+    public:
+        Animation(Transform start, Transform target);
+
+        Transform advance(double elapsed_sec);
+
+        [[nodiscard]] bool active() const;
+        [[nodiscard]] bool reversed() const;
+
+        void activate();
+        void reverse();
+    };
+}
