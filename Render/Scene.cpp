1718859b3a0ce078d1816e67d57bf5dd683155f6
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-11-27 17:48:04 +0000

    Add Scene and Scene builder types
    Scene owns the camera, meshes and shader programs and manages them via raii
    Add setupScene, beforeRender and afterRender hooks

---
+++ Render/Scene.cpp
@@ -0,0 +1,46 @@
+﻿#include "Scene.h"
+
+
+#include <cassert>
+#include <filesystem>
+
+namespace render
+{
+    Scene::Scene(Builder&& builder)
+        : meshes_ {std::move(builder.meshes)},
+          shaders_ {std::move(builder.shaders)},
+          camera_ {*std::move(builder.camera)},
+          root_ {std::move(builder.root)}
+    { }
+
+    Scene Scene::setup([[maybe_unused]] engine::GlInit gl_init)
+    {
+        Builder b;
+        config::hooks::setupScene(b);
+        assert(b.root.get() != nullptr);
+        assert(b.camera.has_value());
+        return b;
+    }
+
+    void Scene::render(engine::Engine& engine, double const elapsed_sec)
+    {
+        config::hooks::beforeRender(*this, engine, elapsed_sec);
+
+        camera_.update(elapsed_sec);
+        root_->update(elapsed_sec);
+        root_->draw(Matrix4::identity(), nullptr);
+
+        config::hooks::afterRender(*this, engine, elapsed_sec);
+    }
+
+    void Scene::animate()
+    {
+        root_->animate();
+    }
+
+    Controller const& Scene::camera() const { return camera_; }
+    Controller&       Scene::camera() { return camera_; }
+
+    Object const& Scene::root() const { return *root_; }
+    Object&       Scene::root() { return *root_; }
+}
---
+++ Render/Scene.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include <deque>
+
+#include "Camera.h"
+#include "Mesh.h"
+#include "Object.h"
+#include "Shader.h"
+#include "../Engine/GlInit.h"
+
+namespace render
+{
+    class Scene
+    {
+    public:
+        struct Builder
+        {
+            Deque<Mesh>          meshes;
+            Deque<ShaderProgram> shaders;
+
+            std::optional<Controller> camera;
+            std::unique_ptr<Object>   root;
+        };
+
+    private:
+        Deque<Mesh>          meshes_;
+        Deque<ShaderProgram> shaders_;
+
+        Controller              camera_;
+        std::unique_ptr<Object> root_;
+
+        Scene(Builder&& builder);
+    public:
+        static Scene setup(engine::GlInit gl_init);
+
+        void render(engine::Engine&, double elapsed_sec);
+        void animate();
+
+        [[nodiscard]] Controller const& camera() const;
+        [[nodiscard]] Controller&       camera();
+
+        [[nodiscard]] Object const& root() const;
+        [[nodiscard]] Object&       root();
+    };
+}
+
+namespace config::hooks
+{
+    void setupScene(render::Scene::Builder& builder);
+    void beforeRender(render::Scene& scene, engine::Engine& engine, double elapsed_sec);
+    void afterRender(render::Scene& scene, engine::Engine& engine, double elapsed_sec);
+}
