7f143530e614eff708885c4879a3e48654a481e2
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-11-27 17:48:45 +0000

    Add Configuration header
    Contains structs for setting the initial parameters for the application

---
+++ Config.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "Utils.h"
+
+namespace config
+{
+    struct WindowSize
+    {
+        int width, height;
+    };
+
+    struct Version
+    {
+        int major, minor;
+    };
+
+    enum class Mode : bool
+    {
+        Windowed = false,
+        Fullscreen = true,
+    };
+
+    enum class VSync: bool
+    {
+        Off = false,
+        On = true,
+    };
+
+    struct Window
+    {
+        Ptr<char const> title;
+        WindowSize  size;
+        Mode        mode;
+        VSync       vsync;
+    };
+
+    struct Settings
+    {
+        Version version;
+        Window  window;
+    };
+}
