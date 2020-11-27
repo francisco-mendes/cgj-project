da7f8fea8bb30348181612b44887efa50953e4d3
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-11-27 17:32:17 +0000

    Add Glfw handle type
    Owns and manages the glfw window and context
    Verifies that glfw was initialized

---
+++ Engine/GlfwHandle.cpp
@@ -0,0 +1,95 @@
+﻿#include "GlfwHandle.h"
+#include <iostream>
+#include <stdexcept>
+
+#include "Error.h"
+
+namespace
+{
+    GLFWwindow* setupWindow(config::Window const& window)
+    {
+        const auto [title, size, fullscreen, vsync] = window;
+
+        auto const monitor = static_cast<bool>(fullscreen) ? glfwGetPrimaryMonitor() : nullptr;
+        auto const win     = glfwCreateWindow(size.width, size.height, title, monitor, nullptr);
+        if (!win)
+            throw std::runtime_error("Unable to create Window");
+
+        glfwMakeContextCurrent(win);
+        glfwSetInputMode(win, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        glfwSetCursor(win, glfwCreateStandardCursor(GLFW_HAND_CURSOR));
+        glfwSwapInterval(static_cast<bool>(vsync));
+
+        return win;
+    }
+}
+
+namespace engine
+{
+    GlfwHandle::GlfwHandle(config::Settings const& settings)
+    {
+        auto const& [version, window] = settings;
+
+        glfwSetErrorCallback(glfwErrorCallback);
+        if (!glfwInit())
+            throw std::runtime_error("Failed to initialize GLFW");
+
+        try
+        {
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, version.major);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, version.minor);
+            glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+
+            window_ = setupWindow(window);
+
+            #if _DEBUG
+            std::cerr << "GLFW " << glfwGetVersionString() << std::endl;
+            #endif
+        }
+        catch (...)
+        {
+            glfwTerminate();
+            throw;
+        }
+    }
+
+    GlfwHandle::GlfwHandle(GlfwHandle&& other) noexcept
+        : window_ {std::exchange(other.window_, nullptr)} {}
+
+    GlfwHandle& GlfwHandle::operator=(GlfwHandle&& other) noexcept
+    {
+        if (this != &other)
+        {
+            window_ = std::exchange(other.window_, nullptr);
+        }
+        return *this;
+    }
+
+    GlfwHandle::~GlfwHandle()
+    {
+        if (window_)
+        {
+            glfwDestroyWindow(window_);
+            glfwTerminate();
+        }
+    }
+
+    double GlfwHandle::getTime() const { return glfwGetTime(); }
+
+    bool GlfwHandle::windowClosing() const { return glfwWindowShouldClose(window_); }
+
+
+    void GlfwHandle::registerEngine(Engine* engine)
+    {
+        callback::setupCallbacks(window_, engine);
+    }
+
+    void GlfwHandle::closeWindow()
+    {
+        callback::onManualWindowClose(window_);
+        glfwSetWindowShouldClose(window_, GLFW_TRUE);
+    }
+
+    void GlfwHandle::swapBuffers() { glfwSwapBuffers(window_); }
+    void GlfwHandle::pollEvents() { glfwPollEvents(); }
+}
---
+++ Engine/GlfwHandle.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "../Config.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+namespace engine
+{
+    class GlfwHandle
+    {
+        friend class Engine;
+
+        GLFWwindow* window_;
+
+    public:
+        [[nodiscard]] explicit GlfwHandle(config::Settings const& settings);
+
+        GlfwHandle(GlfwHandle const&)            = delete;
+        GlfwHandle& operator=(GlfwHandle const&) = delete;
+
+        GlfwHandle(GlfwHandle&& other) noexcept;
+        GlfwHandle& operator=(GlfwHandle&& other) noexcept;
+
+        ~GlfwHandle();
+
+        [[nodiscard]] double getTime() const;
+        [[nodiscard]] bool   windowClosing() const;
+
+    private:
+        void registerEngine(Engine* engine);
+        void closeWindow();
+        void swapBuffers();
+        void pollEvents();
+    };
+}
