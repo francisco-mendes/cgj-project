21d8c948c293790c0a559d51d82488c4fce25b28
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-11-27 17:43:05 +0000

    Add Object type
    Objects can be drawn if they have meshes
    Objects can be animated between two transformations
    Objects may have children, which they own and manage via raii

---
+++ Render/Object.cpp
@@ -0,0 +1,57 @@
+﻿#include "Object.h"
+
+#include <cassert>
+
+
+namespace render
+{
+    Object::Object(OptPtr<Mesh const> const mesh, Vector4 const color, OptPtr<ShaderProgram const> const shaders)
+        : mesh {mesh},
+          shaders {shaders},
+          color {color}
+    {}
+
+    Object::Object(OptPtr<ShaderProgram const> const shaders)
+        : mesh {nullptr},
+          shaders {shaders},
+          color {}
+    {}
+
+    void Object::animate()
+    {
+        if (animation)
+        {
+            if (animation->active()) { animation->reverse(); }
+            else { animation->activate(); }
+        }
+    }
+
+    void Object::update(double const elapsed_sec)
+    {
+        if (animation && animation->active())
+            transform = animation->advance(elapsed_sec);
+
+        for (auto& child : children) { child->update(elapsed_sec); }
+    }
+
+    void Object::draw(Matrix4 const& parent_transform, OptPtr<ShaderProgram const> const parent_shaders) const
+    {
+        auto const shaders = this->shaders != nullptr ? this->shaders : parent_shaders;
+        assert(shaders != nullptr);
+
+        if (shaders != parent_shaders) { glUseProgram(shaders->programId()); }
+        {
+            auto const model_matrix = parent_transform * transform.toMatrix();
+
+            if (auto const [r, g, b, alpha] = color; mesh != nullptr && alpha != 0)
+            {
+                glBindVertexArray(mesh->vaoId());
+                glUniform4f(shaders->colorId(), r, g, b, alpha);
+                glUniformMatrix4fv(shaders->modelId(), 1, GL_TRUE, model_matrix.inner);
+                glDrawArrays(GL_TRIANGLES, 0, mesh->vertexCount());
+            }
+            for (auto const& child : children) { child->draw(model_matrix, shaders); }
+        }
+        if (shaders != parent_shaders && parent_shaders != nullptr) { glUseProgram(parent_shaders->programId()); }
+    }
+}
---
+++ Render/Object.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include <deque>
+#include <vector>
+
+#include "Mesh.h"
+#include "Shader.h"
+#include "Transform.h"
+#include "../Utils.h"
+
+namespace render
+{
+    template <class Type>
+    using Deque = std::pmr::deque<Type>;
+
+    struct Object
+    {
+        [[nodiscard]] explicit Object(
+            OptPtr<Mesh const>          mesh,
+            Vector4                     color,
+            OptPtr<ShaderProgram const> shaders = nullptr
+        );
+
+        [[nodiscard]] explicit Object(OptPtr<ShaderProgram const> shaders = nullptr);
+
+        void animate();
+        void update(double elapsed_sec);
+        void draw(Matrix4 const& parent_transform, OptPtr<ShaderProgram const> parent_shaders) const;
+
+        OptPtr<Mesh const>          mesh;
+        OptPtr<ShaderProgram const> shaders;
+
+        Vector4       color;
+        Transform     transform;
+        Deque<Object> children;
+
+        std::optional<Animation> animation;
+    };
+}
