5e9bf52fb6676ae6bbb9b3aac7f4d692f519cff0
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-11-27 17:29:52 +0000

    Add Utility header
    Add templates for nullable and non nullable, non owning pointers.
    Add logTimeTaken template functions to measure time taken during tasks, like loading assets

---
+++ Utils.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <chrono>
+#include <iostream>
+
+template <class T>
+using OptPtr = T*;
+
+template <class T>
+using Ptr = T*;
+
+template <bool Pred, class F>
+using ReturnsVoid = std::enable_if_t<std::is_void_v<std::invoke_result_t<F>> == Pred>;
+
+template <class Run, typename = ReturnsVoid<false, Run>>
+auto logTimeTaken(std::string_view const name, Run run)
+{
+    using namespace std::chrono;
+    auto const start = steady_clock::now();
+    auto       temp  = run();
+    auto const end   = steady_clock::now();
+    auto const diff  = end - start;
+    std::cerr << name << " took " << duration_cast<milliseconds>(diff).count() << " ms." << std::endl;
+    return temp;
+}
+
+template <class Run, typename = ReturnsVoid<true, Run>>
+void logTimeTaken(std::string_view const name, Run run)
+{
+    using namespace std::chrono;
+    auto const start = steady_clock::now();
+    run();
+    auto const end  = steady_clock::now();
+    auto const diff = end - start;
+    std::cerr << name << " took " << duration_cast<milliseconds>(diff).count() << " ms." << std::endl;
+}
+
