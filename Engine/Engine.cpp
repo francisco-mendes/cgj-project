bdd1ba05191c42f1b2ff90f72b921f4fcd3453ff
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-11-27 17:34:16 +0000

    Add Engine type
    Manages all resources for the application
    Must have a stable location for callbacks, cannot be moved
    Runs the event and rendering loop

---
+++ Engine/Engine.cpp
@@ -0,0 +1,43 @@
+﻿#include "Engine.h"
+
+#include "Error.h"
+
+namespace engine
+{
+    Engine::Engine(GlfwHandle glfw, render::Scene scene)
+        : glfw_ {std::move(glfw)},
+          scene_ {std::move(scene)}
+    {
+        glfw_.registerEngine(this);
+        setupErrorCallback(this);
+    }
+
+    std::unique_ptr<Engine> Engine::init(GlfwHandle glfw, render::Scene scene)
+    {
+        return std::unique_ptr<Engine> {new Engine(std::move(glfw), std::move(scene))};
+    }
+
+    Ptr<GLFWwindow> Engine::window() { return glfw_.window_; }
+    render::Scene&  Engine::scene() { return scene_; }
+
+    void Engine::run()
+    {
+        auto last_time = glfw_.getTime();
+
+        while (!glfw_.windowClosing())
+        {
+            auto const now   = glfw_.getTime();
+            auto const delta = now - last_time;
+            last_time        = now;
+
+            // Double Buffers
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            scene_.render(*this, delta);
+
+            glfw_.swapBuffers();
+            glfw_.pollEvents();
+        }
+    }
+
+    void Engine::terminate() { glfw_.closeWindow(); }
+}
---
+++ Engine/Engine.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "GlfwHandle.h"
+#include "../Render/Scene.h"
+
+namespace engine
+{
+    class Engine
+    {
+        GlfwHandle    glfw_;
+        render::Scene scene_;
+
+        Engine(GlfwHandle glfw, render::Scene scene);
+    public:
+        static std::unique_ptr<Engine> init(GlfwHandle glfw, render::Scene scene);
+
+        Engine(Engine const&)            = delete;
+        Engine& operator=(Engine const&) = delete;
+        Engine(Engine&&)                 = delete;
+        Engine& operator=(Engine&&)      = delete;
+
+        [[nodiscard]] Ptr<GLFWwindow> window();
+        [[nodiscard]] render::Scene&  scene();
+
+        void run();
+        void terminate();
+    };
+}
