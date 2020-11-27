10073ded63c1b501f2148f78f5f649948f1f4c13
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-11-27 17:49:52 +0000

    Add Callbacks
    Add callback input types
    Add callback hooks
    Add callback setup and cleanup on manual close

---
+++ Callback.cpp
@@ -0,0 +1,62 @@
+﻿#include "Callback.h"
+
+namespace
+{
+    using namespace config::hooks;
+    
+    [[nodiscard]] engine::Engine* getEngine(GLFWwindow* win)
+    {
+        return static_cast<engine::Engine*>(glfwGetWindowUserPointer(win));
+    }
+
+    void windowCloseCallback(GLFWwindow* win)
+    {
+        onWindowClose(*getEngine(win));
+    }
+
+    void windowSizeCallback(GLFWwindow* win, int const width, int const height)
+    {
+        onWindowResize(*getEngine(win), {width, height});
+    }
+
+    void mouseCallback(GLFWwindow* win, int const button, int const action, int const mods)
+    {
+        onMouseButton(*getEngine(win), {button, action, mods});
+    }
+
+    void cursorCallback(GLFWwindow* win, double const x_pos, double const y_pos)
+    {
+        onMouseMove(*getEngine(win), {x_pos, y_pos});
+    }
+
+    void scrollCallback(GLFWwindow* win, [[maybe_unused]] double const _, double const y_pos)
+    {
+        onMouseScroll(*getEngine(win), y_pos);
+    }
+
+    void keyCallback(GLFWwindow* win, int const key, int const scancode, int const action, int const mods)
+    {
+        onKeyboardButton(*getEngine(win), {key, scancode, action, mods});
+    }
+}
+
+namespace callback
+{
+    MousePosition::operator Vector2() const { return {static_cast<float>(x), static_cast<float>(y)}; }
+
+    void setupCallbacks(GLFWwindow* window, engine::Engine* engine)
+    {
+        glfwSetWindowCloseCallback(window, windowCloseCallback);
+        glfwSetWindowSizeCallback(window, windowSizeCallback);
+
+        glfwSetMouseButtonCallback(window, mouseCallback);
+        glfwSetScrollCallback(window, scrollCallback);
+        glfwSetCursorPosCallback(window, cursorCallback);
+
+        glfwSetKeyCallback(window, keyCallback);
+
+        glfwSetWindowUserPointer(window, engine);
+    }
+
+    void onManualWindowClose(GLFWwindow* window) { windowCloseCallback(window); }
+}
---
+++ Callback.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "Math/Vector.h"
+
+#include <Gl/glew.h>
+#include <GLFW/glfw3.h>
+
+namespace engine
+{
+    class Engine; // Forward declaration to avoid circular dependencies
+}
+
+namespace callback
+{
+    struct WindowSize
+    {
+        int width, height;
+    };
+
+    struct MouseButton
+    {
+        int button, action, mods;
+    };
+
+    struct MousePosition
+    {
+        double x, y;
+
+        operator Vector2() const;
+    };
+
+    struct KeyboardButton
+    {
+        int key, scancode, action, mods;
+    };
+
+    void setupCallbacks(GLFWwindow* window, engine::Engine* engine);
+    void onManualWindowClose(GLFWwindow* window);
+}
+
+namespace config::hooks
+{
+    void onWindowClose(engine::Engine& engine);
+    void onWindowResize(engine::Engine& engine, callback::WindowSize size);
+
+    void onMouseButton(engine::Engine& engine, callback::MouseButton button);
+    void onMouseMove(engine::Engine& engine, callback::MousePosition position);
+    void onMouseScroll(engine::Engine& engine, double scroll);
+
+    void onKeyboardButton(engine::Engine& engine, callback::KeyboardButton button);
+}
