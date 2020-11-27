cf57e5d33e1bba5c74cb667aad9f0e95ff935693
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2020-11-27 17:26:20 +0000

    Update Shader implementation
    Use filesystem paths for loading
    Reimplement lifetime semantics for Shader and ShaderProgram
    Implement move operators and destructors
    Move ShaderProgram implementation to cpp
    Implement equality operators for ShaderProgram

---
+++ Render/Shader.cpp
@@ -0,0 +1,174 @@
+﻿#include "Shader.h"
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+namespace render
+{
+    namespace
+    {
+        void checkCompilation(GLuint const shader_id)
+        {
+            GLint compiled;
+            glGetShaderiv(shader_id, GL_COMPILE_STATUS, &compiled);
+            if (compiled == GL_FALSE)
+            {
+                GLint length;
+                glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &length);
+
+                std::string log = "Failed to compile shader: ";
+                log.resize(length + log.size());
+                glGetShaderInfoLog(shader_id, length, &length, log.data());
+                throw std::runtime_error(log);
+            }
+        }
+
+        void checkLinkage(const GLuint program_id)
+        {
+            GLint linked;
+            glGetProgramiv(program_id, GL_LINK_STATUS, &linked);
+            if (linked == GL_FALSE)
+            {
+                GLint length;
+                glGetProgramiv(program_id, GL_INFO_LOG_LENGTH, &length);
+
+                std::string log = "Failed to link program: ";
+                log.resize(length + log.size());
+                glGetProgramInfoLog(program_id, length, &length, log.data());
+                throw std::runtime_error(log);
+            }
+        }
+
+        std::string readShaderFile(std::filesystem::path const& shader_path)
+        {
+            if (!exists(shader_path))
+                throw std::invalid_argument("File not found: " + shader_path.string());
+
+            auto const size = file_size(shader_path);
+
+            std::ifstream in;
+            in.exceptions(std::ios_base::badbit);
+            in.open(shader_path);
+
+            std::string program;
+            program.resize(size);
+
+            in.read(program.data(), size);
+            return program;
+        }
+    }
+
+    Shader::Shader(Type const shader_type, std::string_view const program)
+        : shader_type_ {shader_type},
+          shader_id_ {glCreateShader(static_cast<GLenum>(shader_type))}
+    {
+        auto const src = program.data();
+        auto const len = static_cast<GLint>(program.length());
+
+        glShaderSource(shader_id_, 1, &src, &len);
+        glCompileShader(shader_id_);
+        checkCompilation(shader_id_);
+    }
+
+    Shader Shader::fromFile(Type const shader_type, std::filesystem::path const& shader_file)
+    {
+        try
+        {
+            auto const program = readShaderFile(shader_file);
+            return Shader {shader_type, program};
+        }
+        catch (...)
+        {
+            std::cerr << "Error loading shader at " << absolute(shader_file) << '.' << std::endl;
+            throw;
+        }
+    }
+
+    Shader::Shader(Shader&& other) noexcept
+        : shader_type_ {other.shader_type_},
+          shader_id_ {std::exchange(other.shader_id_, 0)} {}
+
+    Shader& Shader::operator=(Shader&& other) noexcept
+    {
+        if (this != &other)
+        {
+            shader_type_ = other.shader_type_;
+            shader_id_   = std::exchange(other.shader_id_, 0);
+        }
+        return *this;
+    }
+
+    Shader::~Shader()
+    {
+        if (shader_id_ != 0)
+        {
+            glDeleteShader(shader_id_);
+        }
+    }
+
+    Shader::Type Shader::type() const { return shader_type_; }
+    GLuint       Shader::id() const { return shader_id_; }
+
+
+    ShaderProgram::ShaderProgram(ShaderProgram&& other) noexcept
+        : program_id_ {std::exchange(other.program_id_, 0)},
+          model_id_ {std::exchange(other.model_id_, 0)},
+          color_id_ {std::exchange(other.color_id_, 0)} {}
+
+    ShaderProgram& ShaderProgram::operator=(ShaderProgram&& other) noexcept
+    {
+        if (this != &other)
+        {
+            program_id_ = std::exchange(other.program_id_, 0);
+            model_id_   = std::exchange(other.model_id_, 0);
+            color_id_   = std::exchange(other.color_id_, 0);
+        }
+        return *this;
+    }
+
+    ShaderProgram::~ShaderProgram()
+    {
+        if (program_id_ != 0)
+        {
+            glDeleteProgram(program_id_);
+        }
+    }
+
+    ShaderProgram::ShaderProgram(std::initializer_list<Shader> shaders)
+        : program_id_ {glCreateProgram()}
+    {
+        for (auto const& shader : shaders)
+        {
+            glAttachShader(program_id_, shader.id());
+        }
+
+        glBindAttribLocation(program_id_, Position, "in_Position");
+        glBindAttribLocation(program_id_, Colors, "in_Color");
+
+        glLinkProgram(program_id_);
+        checkLinkage(program_id_);
+
+        model_id_ = glGetUniformLocation(program_id_, "ModelMatrix");
+        color_id_ = glGetUniformLocation(program_id_, "Color");
+
+        auto const camera_id = glGetUniformBlockIndex(program_id_, "CameraMatrices");
+        glUniformBlockBinding(program_id_, camera_id, Camera);
+
+        for (auto const& shader : shaders)
+        {
+            glDetachShader(program_id_, shader.id());
+        }
+    }
+
+    GLuint ShaderProgram::programId() const { return program_id_; }
+    GLuint ShaderProgram::modelId() const { return model_id_; }
+    GLuint ShaderProgram::colorId() const { return color_id_; }
+}
+
+bool operator==(render::ShaderProgram const& lhs, render::ShaderProgram const& rhs)
+{
+    return lhs.programId() == rhs.programId();
+}
+
+bool operator!=(render::ShaderProgram const& lhs, render::ShaderProgram const& rhs) { return !(lhs == rhs); }
---
+++ Render/Shader.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+
+#include <filesystem>
+#include <optional>
+
+#include <GL/glew.h>
+
+#include "Shader.h"
+
+namespace render
+{
+    class Shader
+    {
+    public:
+        enum Type : GLenum
+        {
+            Vertex = GL_VERTEX_SHADER,
+            TesselationControl = GL_TESS_CONTROL_SHADER,
+            TesselationEvaluation = GL_TESS_EVALUATION_SHADER,
+            Geometry = GL_GEOMETRY_SHADER,
+            Fragment = GL_FRAGMENT_SHADER,
+            Compute = GL_COMPUTE_SHADER
+        };
+
+    private:
+        Type   shader_type_;
+        GLuint shader_id_;
+
+    public:
+        Shader(Type shader_type, std::string_view);
+        static Shader fromFile(Type shader_type, std::filesystem::path const& shader_file);
+
+        Shader(Shader const&)            = delete;
+        Shader& operator=(Shader const&) = delete;
+
+        Shader(Shader&& other) noexcept;
+        Shader& operator=(Shader&&) noexcept;
+
+        ~Shader();
+
+        [[nodiscard]] Type   type() const;
+        [[nodiscard]] GLuint id() const;
+    };
+
+    class ShaderProgram
+    {
+    public:
+        constexpr static GLuint Position = 0;
+        constexpr static GLuint Colors   = 1;
+
+        constexpr static GLuint Camera = 0;
+    private:
+        GLuint program_id_, model_id_, color_id_;
+    public:
+        ShaderProgram(ShaderProgram const&)            = delete;
+        ShaderProgram& operator=(ShaderProgram const&) = delete;
+
+        ShaderProgram(ShaderProgram&& other) noexcept;
+        ShaderProgram& operator=(ShaderProgram&&) noexcept;
+
+        ShaderProgram(std::initializer_list<Shader> shaders);
+
+        ~ShaderProgram();
+
+        template <class... Shaders>
+        explicit ShaderProgram(Shaders ...shaders) : ShaderProgram({std::move(shaders) ...})
+        {
+            static_assert((std::is_same_v<Shader, Shaders> && ...));
+        }
+
+        [[nodiscard]] GLuint programId() const;
+        [[nodiscard]] GLuint modelId() const;
+        [[nodiscard]] GLuint colorId() const;
+    };
+}
+
+bool operator==(render::ShaderProgram const& lhs, render::ShaderProgram const& rhs);
+bool operator!=(render::ShaderProgram const& lhs, render::ShaderProgram const& rhs);
