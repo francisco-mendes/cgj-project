3f63fc2b17a0c7f9d55c1d725b371ea41683d60f
Lucas Silva <lukaniaco@gmail.com>
2020-12-20 15:30:31 +0000

    celShading

---
+++ Shaders/CelFrag.glsl
@@ -0,0 +1,34 @@
+﻿#version 330 core
+
+in vec3 ex_Position;
+in vec2 ex_Texcoord;
+in vec3 ex_Normal;
+
+in vec4 ex_Color;
+out vec4 out_Color;
+
+in vec3 LightPos;
+
+void main(void)
+{
+	vec3 light_pos = vec3(10, 50,10);
+  	vec3 lightDir = -normalize(ex_Position - light_pos);
+	vec3 D = normalize(lightDir);
+    vec3 N = normalize(ex_Normal);
+
+    vec3 eye=vec3(0,20,50);
+    eye=normalize(eye);
+
+	float intensity =  dot(D,N);
+	if(intensity>0.95){
+		intensity=1;
+	}else if(intensity>0.5){
+		intensity=0.6;
+	}else if(intensity>0.25){
+		intensity=0.4;
+	}else{
+		intensity=0.2;
+	}
+	if(dot(eye,N)<0.3){intensity=0;}
+	out_Color = ex_Color * intensity;
+}
---
+++ Shaders/CelVert.glsl
@@ -0,0 +1,29 @@
+﻿#version 330 core
+
+in vec3 in_Position;
+in vec2 in_Texcoord;
+in vec3 in_Normal;
+
+out vec3 ex_Position;
+out vec2 ex_Texcoord;
+out vec3 ex_Normal;
+out vec4 ex_Color;
+
+uniform vec4 Color;
+uniform mat4 ModelMatrix;
+
+uniform CameraMatrices
+{
+    mat4 ViewMatrix;
+    mat4 ProjectionMatrix;
+};
+
+void main(void)
+{
+	ex_Texcoord = in_Texcoord;
+	ex_Normal = normalize(inverse(transpose(mat3(ModelMatrix)))*in_Normal);
+	ex_Position = vec3(ModelMatrix*vec4(in_Position,1));
+	gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * vec4(in_Position, 1);
+	ex_Color = Color;
+}
+
