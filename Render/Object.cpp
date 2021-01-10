afc2f6606a3b5edb594274f45713b50a037ae28c
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2021-01-10 19:54:42 +0000

    Update Object code
    Add parent field and constructor argument
    Add emplace and remove operations on Object to add and remove child objects
    Update Object::draw to account for dynamic light positions as well as textures

--- Render/Object.cpp
+++ Render/Object.cpp
@@ -6,16 +6,28 @@
 
 namespace render
 {
-    Object::Object(OptPtr<Mesh const> const mesh, Vector4 const color, OptPtr<Pipeline const> const shaders)
-        : mesh {mesh},
-          shaders {shaders},
-          color {color}
+    Object::Object(
+        OptPtr<Object>               parent,
+        OptPtr<Mesh const> const     mesh,
+        Vector4 const                color,
+        OptPtr<Pipeline const> const shaders,
+        OptPtr<Texture const> const  texture
+    ) : parent {parent},
+        mesh {mesh},
+        shaders {shaders},
+        texture {texture},
+        color {color}
     {}
 
-    Object::Object(OptPtr<Pipeline const> const shaders)
-        : mesh {nullptr},
-          shaders {shaders},
-          color {}
+    Object::Object(
+        OptPtr<Object> const         parent,
+        OptPtr<Pipeline const> const shaders,
+        OptPtr<Texture const> const  texture
+    ) : parent {parent},
+        mesh {nullptr},
+        shaders {shaders},
+        texture {texture},
+        color {Vector4::filled(1.0f)}
     {}
 
     void Object::animate()
@@ -37,9 +49,9 @@ namespace render
     }
 
     void Object::draw(
-        Matrix4 const& parent_transform,
+        Matrix4 const&               parent_transform,
         OptPtr<Pipeline const> const parent_shaders,
-        Scene const& scene
+        Scene const&                 scene
     ) const
     {
         auto const shaders = this->shaders != nullptr ? this->shaders : parent_shaders;
@@ -51,15 +63,53 @@ namespace render
 
             if (auto const [r, g, b, alpha] = color; mesh != nullptr && alpha != 0)
             {
-                auto const [x, y, z] = scene.controller().camera().position();
-                glBindVertexArray(mesh->vaoId());
                 glUniform4f(shaders->colorId(), r, g, b, alpha);
-                glUniform3f(shaders->eyeId(), x, y, z);
+
+                auto const [ex, ey, ez] = scene.camera_controller.camera.position();
+                glUniform3f(shaders->eyeId(), ex, ey, ez);
+
+                auto const [lx, ly, lz] = scene.light_position;
+                glUniform3f(shaders->lightId(), lx, ly, lz);
+
+                auto const texture_bind = texture != nullptr ? texture->texId() : scene.defaultTexture().texId();
+                glUniform1i(shaders->textureId(), 0);
+                glActiveTexture(GL_TEXTURE0);
+                glBindTexture(GL_TEXTURE_2D, texture_bind);
+
                 glUniformMatrix4fv(shaders->modelId(), 1, GL_TRUE, model_matrix.inner);
+                glBindVertexArray(mesh->vaoId());
                 glDrawArrays(GL_TRIANGLES, 0, mesh->vertexCount());
             }
             for (auto const& child : children) { child.draw(model_matrix, shaders, scene); }
         }
         if (shaders != parent_shaders && parent_shaders != nullptr) { glUseProgram(parent_shaders->programId()); }
+    }
+
+    Object& Object::emplaceChild(
+        OptPtr<Mesh const>     mesh,
+        Vector4                color,
+        OptPtr<Pipeline const> shaders,
+        OptPtr<Texture const>  texture
+    )
+    {
+        return children.emplace_back(this, mesh, color, shaders, texture);
+    }
+
+    Object& Object::emplaceChild(OptPtr<Pipeline const> shaders, OptPtr<Texture const> texture)
+    {
+        return children.emplace_back(this, shaders, texture);
+    }
+
+    bool Object::removeChild(Ptr<Object> const child)
+    {
+        for (auto iter = children.begin(), end = children.end(); iter == end; ++iter)
+        {
+            if (&*iter == child)
+            {
+                children.erase(iter);
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Render/Object.h
+++ Render/Object.h
@@ -2,6 +2,7 @@
 
 #include "Mesh.h"
 #include "Shader.h"
+#include "Texture.h"
 #include "Transform.h"
 #include "../Utils.h"
 
@@ -12,24 +13,47 @@ namespace render
     struct Object
     {
         [[nodiscard]] explicit Object(
+            OptPtr<Object>         parent,
             OptPtr<Mesh const>     mesh,
             Vector4                color,
-            OptPtr<Pipeline const> shaders = nullptr
+            OptPtr<Pipeline const> shaders = nullptr,
+            OptPtr<Texture const>  texture = nullptr
         );
 
-        [[nodiscard]] explicit Object(OptPtr<Pipeline const> shaders = nullptr);
+        [[nodiscard]] explicit Object(
+            OptPtr<Object>         parent  = nullptr,
+            OptPtr<Pipeline const> shaders = nullptr,
+            OptPtr<Texture const>  texture = nullptr
+        );
 
         void animate();
         void update(double elapsed_sec);
         void draw(Matrix4 const& parent_transform, OptPtr<Pipeline const> parent_shaders, Scene const& scene) const;
 
+        Object& emplaceChild(
+            OptPtr<Mesh const>     mesh,
+            Vector4                color,
+            OptPtr<Pipeline const> shaders = nullptr,
+            OptPtr<Texture const>  texture = nullptr
+        );
+
+        Object& emplaceChild(
+            OptPtr<Pipeline const> shaders = nullptr,
+            OptPtr<Texture const>  texture = nullptr
+        );
+
+        bool removeChild(Ptr<Object> child);
+
+        OptPtr<Object> parent;
+
         OptPtr<Mesh const>     mesh;
         OptPtr<Pipeline const> shaders;
+        OptPtr<Texture const>  texture;
 
-        Vector4           color;
-        Transform         transform;
-        std::list<Object> children;
+        Vector4   color;
+        Transform transform;
 
         std::optional<Animation> animation;
+        std::list<Object>        children;
     };
 }
